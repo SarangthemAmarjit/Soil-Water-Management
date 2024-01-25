91d36c4d6e3ad926c41432036cf35cb25e29c0b5
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2024-01-25 20:09:34 +0530

    Updated branding

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 // import 'dart:isolate';
 
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:soilmoisturedetector/pages/demo.dart';
-// import 'package:get/get.dart';
-// import 'package:soilmoisturedetector/controller/tapcontroller.dart';
-// import 'package:soilmoisturedetector/pages/demo.dart';
-// import 'package:soilmoisturedetector/demo.dart';
-// import 'package:soilmoisturedetector/pages/dashboard.dart';
-import 'package:soilmoisturedetector/router/router.dart';
-import 'package:soilmoisturedetector/services/alarmmanager.dart';
-import 'package:soilmoisturedetector/services/backgroundservoces.dart';
-// import 'package:soilmoisturedetector/services/backgroundservoces.dart';
 
-// import 'package:soilmoisturedetector/services/workmanager.dart';
+import 'package:soilmoisturedetector/router/router.dart';
+
+import 'package:soilmoisturedetector/services/backgroundservoces.dart';
+
 import 'package:soilmoisturedetector/services/localnotification.dart';
-import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() async {
-  int alarmId = 1;
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await NotificationService().initNotification();
